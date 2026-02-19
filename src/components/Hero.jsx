65eec39561de257244c6ae67db0e861ec7cc3d4b
AryanYadav09 <aryanydv223@gmail.com>
2026-02-19 23:07:42 +0530

    bug fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -262,8 +262,8 @@ const Hero = () => {
           G<b>A</b>MING
         </h1>
 
-        <div className="absolute left-0 top-0 z-40 size-full">
-          <div className="mt-24 px-5 sm:px-10">
+        <div className="pointer-events-none absolute left-0 top-0 z-[60] size-full">
+          <div className="pointer-events-auto mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               redefi<b>n</b>e
             </h1>
